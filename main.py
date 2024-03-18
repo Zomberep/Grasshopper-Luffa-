97025771a104e1bb6ce0d675e9452a5825a7754e
zomberep <zomberep05@gmail.com>
2024-03-18 18:25:54 +0300

    Third

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+# w = 4
 
+def amount_of_zeros(l):
+    return 255 - (l % 256)
+
+def Add(a, b):
+    return a ^ b
+
+def M(a, b):
+    return multi.index((multi[a] + multi[b]) % 255) * (a != 0) * (b != 0)
